ec1fc426039b67678ea64b4946ccdc134d59b6a4
Hannah Lu <hannah.lu@yale.edu>
2025-12-16 18:07:20 -0500

    initial setup

---
+++ 01_data/courtlistener_download.R
@@ -0,0 +1,6 @@
+# NEPA Court Cases
+# This script downloads court cases via the CourtListener API
+# Hannah Lu
+# hannah.lu@yale.edu
+
+# ------------------------------------------------------------------------------
